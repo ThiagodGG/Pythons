9ab5cf66f3e2d707716efdd20de0c0e49374ceba
Thiago Barboza <thiagoafonsobarboza@gmail.com>
2025-10-28 18:12:58 +0000

    Alguns codigos e aprendizados em py

---
+++ PythonsINI/Conteudos/EstruturaFor.py
@@ -0,0 +1,26 @@
+for c in range(0, 7):
+    print(c)
+print('Fim')
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('Fim')
+
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('Fim!')
+
+for c in range(0, 3):
+    n = int(input('Digite um valor: '))
+print('Fim!')
+
+s = 0
+for c in range(0, 4):
+    n = int(input("Digite outro valor: "))
+    s += n
+print('A soma de todos os valores foi {}'.format(s))
+
